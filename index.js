fa4a7294ba4c8e42d7aabf8856fde305257ea8bc
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-09-06 12:45:18 -0400

    commit tree aab81097d2667c46b531212a95ef93c3308fa065

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatteries =  batteryBatches
+// let number = 0;
+// batteryBatches.forEach(totalBatteries => {
+//   number = number + totalBatteries.number
+// })
+// console.log("NUMBER :",  number)
+
+// const totalBatteries =  batteryBatches
+//  let sum = [4, 5, 3, 4, 4, 6, 5].reduce(function(previousValue, currentValue){
+//   return previousValue + currentValue
+//  }, 0)
+//  console.log("SUM:", sum)
+
+
+const totalBatteries = batteryBatches.reduce((total , item)=> total + item
+ )
+// console.log (totalPrice)
